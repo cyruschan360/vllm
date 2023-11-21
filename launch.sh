c62907398d70295809608920297463b4b0bb96de
Cyrus Chan <cyrus.chan@star.hku.hk>
2023-11-21 13:15:20 +0800

    Added launch script

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#pip install autoawq
+
+#nohup bash ./start.sh &
+#disown -h
+
+python -m vllm.entrypoints.openai.api_server \
+  --host 0.0.0.0 \
+  --port 3000 \
+  --model TheBloke/Llama-2-70B-chat-AWQ \
+  --quantization awq \
+  --tensor-parallel-size 2 \
+  --trust-remote-code
+
